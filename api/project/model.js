35c55ef663acd6b09622ec13072452d7ca0076c9
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-07-16 13:47:21 -0500

    updated project model to hammer data together for client response

--- api/project/model.js
+++ api/project/model.js
@@ -1,8 +1,25 @@
 // build your `Project` model here
 const db = require('../../data/dbConfig')
 
-function getProjects() {
-  return db('projects')
+async function getProjects() {
+  const rows = await db('projects')  // this is an array of records
+  const formattedCollection = []
+  rows.forEach(record => {
+    if(record.project_completed === 0){
+      record.project_completed = false
+    } else if(record.project_completed === 1){
+      record.project_completed = true
+    }
+
+    formattedCollection.push({
+      project_id: record.project_id,
+      project_name: record.project_name,
+      project_description: record.project_description,
+      project_completed: record.project_completed
+    })
+  })
+
+  return formattedCollection
 }
 
 
